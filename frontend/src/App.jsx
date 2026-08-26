3ecef0bfa10a386859b44cf0e382f2b95f39fada
Himanshu Singh <279612130+obrahimanshu14-collab@users.noreply.github.com>
2026-08-26 19:03:46 +0530

    Add public product information routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import DashboardPage from "./pages/DashboardPage";
 import HistoryPage from "./pages/HistoryPage";
 import LiveDetectionPage from "./pages/LiveDetectionPage";
 import AdminPage from "./pages/AdminPage";
+import TechnologyPage from "./pages/TechnologyPage";
+import ResearchPage from "./pages/ResearchPage";
+import HowItWorksPage from "./pages/HowItWorksPage";
+import SecurityPage from "./pages/SecurityPage";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/technology" element={<TechnologyPage />} />
+        <Route path="/research" element={<ResearchPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/security" element={<SecurityPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
