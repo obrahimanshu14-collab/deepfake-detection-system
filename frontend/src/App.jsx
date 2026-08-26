61462afd3f10b2cf65d5da2935450e94179e6874
Himanshu Singh <279612130+obrahimanshu14-collab@users.noreply.github.com>
2026-08-27 02:45:48 +0530

    Add persistent product footer to application shell

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 import LandingPage from "./pages/LandingPage";
 import SignupPage from "./pages/SignupPage";
@@ -25,16 +26,15 @@ function App() {
         <Route path="/security" element={<SecurityPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/history" element={<HistoryPage />} />
           <Route path="/live" element={<LiveDetectionPage />} />
           <Route path="/admin" element={<AdminPage />} />
         </Route>
-
         <Route path="*" element={<LandingPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
