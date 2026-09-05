bd618d491fdeb20500516dcf10cd7861a7a3ef32
Himanshu Singh <279612130+obrahimanshu14-collab@users.noreply.github.com>
2026-09-05 19:40:01 +0530

    Add protected developer platform route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminPage from "./pages/AdminPage";
 import DashboardPage from "./pages/DashboardPage";
+import DeveloperPage from "./pages/DeveloperPage";
 import HistoryPage from "./pages/HistoryPage";
 import LandingPage from "./pages/LandingPage";
 import LiveDetectionPage from "./pages/LiveDetectionPage";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
         <Route path="/history" element={<ProtectedRoute><HistoryPage /></ProtectedRoute>} />
+        <Route path="/developers" element={<ProtectedRoute><DeveloperPage /></ProtectedRoute>} />
         <Route path="/live" element={<ProtectedRoute><LiveDetectionPage /></ProtectedRoute>} />
         <Route path="/upgrade" element={<ProtectedRoute><UpgradePage /></ProtectedRoute>} />
         <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>} />
